518a42927c3ae5231b72c56dbf3fa41863a432cb
Alex Wears <alexanderwears02@gmail.com>
2023-05-27 11:16:41 -0500

    Refactored in accordance with DRY

--- spoti.py
+++ spoti.py
@@ -15,22 +15,21 @@ from pygame.locals import *
 color = (20,20,20)
 
 def display_current_pygame_image(track, scrn, rect):
+    # Get album art
+    py_image = get_current_pygame_image(track)
 
-    album_art =  Image.open(urlopen(track["item"]["album"]["images"][0]["url"]))
-    mode = album_art.mode
-    size = album_art.size
-    data = album_art.tobytes()
-    py_image = pygame.image.fromstring(data, size, mode)
-
+    # Paint the screen
     scrn.fill(color)
     scrn.blit(pygame.transform.scale(py_image, (rect.width, rect.height)), rect)
     pygame.display.flip()
 
 def display_blank_screen(scrn):
+    # Fill screen with default color and paint it
     scrn.fill(color)
     pygame.display.flip()
 
 def get_current_pygame_image(track):
+    # Retrieve ablum art and convert it to a usable image
     album_art =  Image.open(urlopen(track["item"]["album"]["images"][0]["url"]))
     mode = album_art.mode
     size = album_art.size
@@ -38,16 +37,53 @@ def get_current_pygame_image(track):
     return pygame.image.fromstring(data, size, mode)
 
 def resize_pygame_image(track, scrn, rect):
+    # Determine scaling factor for the album art
     scale = 1
     if scrn.get_height() < scrn.get_width():
         scale = scrn.get_height() / rect.height
     else:
         scale = scrn.get_width() / rect.width
     
+    # Scale the art and display it
     rect.height = rect.height * scale
     rect.width = rect.width * scale
     rect.center = scrn.get_rect().center
     display_current_pygame_image(track, scrn, rect)
+
+def get_input():
+    for event in pygame.event.get():
+        # Quit
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+
+        # Track mouse position for song controls
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if pos[0] < scrn.get_width() / 3:
+                spotipy.Spotify(token).previous_track()
+            elif pos[0] < (scrn.get_width() / 3) * 2:
+                try:
+                    spotipy.Spotify(token).pause_playback()
+                except Exception as e:
+                    spotipy.Spotify(token).start_playback()
+            else:
+                spotipy.Spotify(token).next_track()
+
+        # Track potential screen resizes and scale art accordingly
+        elif event.type == VIDEORESIZE:
+            scale = 1
+            if scrn.get_height() < scrn.get_width():
+                scale = scrn.get_height() / rect.height
+            else:
+                scale = scrn.get_width() / rect.width
+            
+            rect.height = rect.height * scale
+            rect.width = rect.width * scale
+            rect.center = scrn.get_rect().center
+            display_current_pygame_image(track, scrn, rect)
 
 if __name__ == '__main__':
 
@@ -96,39 +132,8 @@ if __name__ == '__main__':
 
             # Continuously check for track updates
             while True:
-                for event in pygame.event.get():
-                    # Quit
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                    elif event.type == KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            pygame.quit()
-
-                    # Track mouse position for song controls
-                    elif event.type == pygame.MOUSEBUTTONDOWN:
-                        pos = pygame.mouse.get_pos()
-                        if pos[0] < scrn.get_width() / 3:
-                            spotipy.Spotify(token).previous_track()
-                        elif pos[0] < (scrn.get_width() / 3) * 2:
-                            try:
-                                spotipy.Spotify(token).pause_playback()
-                            except Exception as e:
-                                spotipy.Spotify(token).start_playback()
-                        else:
-                            spotipy.Spotify(token).next_track()
-
-                    # Track potential screen resizes and scale art accordingly
-                    elif event.type == VIDEORESIZE:
-                        scale = 1
-                        if scrn.get_height() < scrn.get_width():
-                            scale = scrn.get_height() / rect.height
-                        else:
-                            scale = scrn.get_width() / rect.width
-                        
-                        rect.height = rect.height * scale
-                        rect.width = rect.width * scale
-                        rect.center = scrn.get_rect().center
-                        display_current_pygame_image(track, scrn, rect)
+                # Track different user inputs
+                get_input()
 
                 # Track stayed the same
                 if track["item"]["id"] == spotipy.Spotify(token).current_user_playing_track()["item"]["id"]:
@@ -151,8 +156,5 @@ if __name__ == '__main__':
         except Exception as e:
             print(e)
             display_blank_screen(scrn)
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.quit()
-            time.sleep(3)
+            get_input()
+            time.sleep(1)
